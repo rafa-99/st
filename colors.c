fa3255736a82b94378a14bb732a3f59739eb6848
rafa_99 <rafa99@protonmail.com>
2020-02-21 14:34:37 +0000

    Modularized Colors for Easy Replacement

---
+++ colors.c
@@ -0,0 +1,27 @@
+static const char *colorname[] = {
+
+  /* 8 normal colors */
+  [0] = "#040404", /* black   */
+  [1] = "#E3E3E3", /* red     */
+  [2] = "#E3E3E3", /* green   */
+  [3] = "#777777", /* yellow  */
+  [4] = "#8E8E8E", /* blue    */
+  [5] = "#B0B0B0", /* magenta */
+  [6] = "#C8C8C8", /* cyan    */
+  [7] = "#e3e3e3", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#9e9e9e",  /* black   */
+  [9]  = "#E3E3E3",  /* red     */
+  [10] = "#E3E3E3", /* green   */
+  [11] = "#777777", /* yellow  */
+  [12] = "#8E8E8E", /* blue    */
+  [13] = "#B0B0B0", /* magenta */
+  [14] = "#C8C8C8", /* cyan    */
+  [15] = "#e3e3e3", /* white   */
+
+  /* special colors */
+  [256] = "#000000", /* background */
+  [257] = "#FFFFFF", /* foreground */
+  [258] = "#FFFFFF", /* cursor */
+};
--- config.h
+++ config.h
@@ -98,34 +98,7 @@ unsigned int tabspaces = 8;
 float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-
-  /* 8 normal colors */
-  [0] = "#040404", /* black   */
-  [1] = "#E3E3E3", /* red     */
-  [2] = "#E3E3E3", /* green   */
-  [3] = "#777777", /* yellow  */
-  [4] = "#8E8E8E", /* blue    */
-  [5] = "#B0B0B0", /* magenta */
-  [6] = "#C8C8C8", /* cyan    */
-  [7] = "#e3e3e3", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#9e9e9e",  /* black   */
-  [9]  = "#E3E3E3",  /* red     */
-  [10] = "#E3E3E3", /* green   */
-  [11] = "#777777", /* yellow  */
-  [12] = "#8E8E8E", /* blue    */
-  [13] = "#B0B0B0", /* magenta */
-  [14] = "#C8C8C8", /* cyan    */
-  [15] = "#e3e3e3", /* white   */
-
-  /* special colors */
-  [256] = "#000000", /* background */
-  [257] = "#FFFFFF", /* foreground */
-  [258] = "#FFFFFF", /* cursor */
-};
-
+#include "colors.c"
 
 /*
  * Default colors (colorname index)
