fc6a088db2125218db62966246615014525df65b
rafa_99 <rafa99@protonmail.com>
2020-03-15 13:34:44 +0000

    Updated Font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font:size=10:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
