4eb2844ca92c161606651f4b86278ac91e94808c
rafa_99 <rafa99@protonmail.com>
2020-03-24 11:57:39 +0000

    Fixed Font Compile Error

--- config.h
+++ config.h
@@ -8,8 +8,7 @@
 static char *font = "JetBrains Mono Nerd Font:size=10:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"Ubuntu Mono Nerd Font:pixelsize=12:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
