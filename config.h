fa8dabacbd6f78ae3c756e6cee5da204de7a70d7
rafa_99 <rafa99@protonmail.com>
2020-04-26 20:49:47 +0100

    Updated Fonts

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font:size=10:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font:size=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Ubuntu Mono Nerd Font:pixelsize=12:antialias=true:autohint=true",
+	"Symbola:size=12:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
