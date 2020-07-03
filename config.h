69045ccf6277a6402787f924c03526be680f8765
rafa_99 <rafa99@protonmail.com>
2020-07-03 22:14:44 +0000

    Updated Cursor

--- config.h
+++ config.h
@@ -116,7 +116,7 @@ float alpha = 0.8;
  * 7: Blinking st cursor
  * 8: Steady st cursor
  */
-static unsigned int cursorstyle = 6;
+static unsigned int cursorstyle = 5;
 static Rune stcursor = 0x2603; /* snowman (U+2603) */
 
 /*
