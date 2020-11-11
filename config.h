62d0a78e4056f258077483bb4b93262b7c060792
rafa_99 <rafa99@protonmail.com>
2020-11-11 17:33:31 +0000

    Updated Cursor to BLOCO

--- config.h
+++ config.h
@@ -116,7 +116,7 @@ float alpha = 1;
  * 7: Blinking st cursor
  * 8: Steady st cursor
  */
-static unsigned int cursorstyle = 5;
+static unsigned int cursorstyle = 1;
 static Rune stcursor = 0x2603; /* snowman (U+2603) */
 
 /*
